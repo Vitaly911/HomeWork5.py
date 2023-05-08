3fcefc0022f2b91874237a6fa9f7ff9ba95b50c3
Vitaly <sinicin1313@gmail.com>
2023-05-08 11:17:31 +0300

    first commit

---
+++ 26.py
@@ -0,0 +1,5 @@
+number1 = int(input('Введите число: '))
+number2 = int(input('Введите число: '))
+def degree (A, B):
+    print(A ** B)
+degree (number1, number2)  
---
+++ 28.py
@@ -0,0 +1,5 @@
+number1 = int(input('Введите число: '))
+number2 = int(input('Введите число: '))
+def sum (A, B):
+    print(A + B)
+sum (number1, number2)
